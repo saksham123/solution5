2a2a63713ccfd246442eb242f2e8dc4088cd9470
saksham rana <0blackbuck@gmail.com>
2019-01-27 21:19:06 -0500

    printpyramid

--- solution5/Program.cs
+++ solution5/Program.cs
@@ -4,9 +4,35 @@ namespace solution5
 {
     class Program
     {
+        public static void printPyramid()
+        {
+            int n, x, blank;
+            Console.WriteLine("Please enter the height of pyramid in number of lines");//input value from user for number of lines
+            n = Int32.Parse(Console.ReadLine()); // convert to integer
+            Console.WriteLine("Enjoy your pyramid");//display message
+            for (int i = 1; i <= n; i++) //till height of pyramid
+            {
+                for (blank = 1; blank <= (n - i); blank++) // insert space
+                    Console.Write(" ");
+                for (x = 1; x <= i; x++) //increment value by 1 each time
+                    Console.Write('*');
+                for (x = (i - 1); x >= 1; x--) //decrement the value by 1
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                printPyramid();//call the method
+            }
+            catch (Exception)
+            {
+                Console.Write("Please enter a valid input");
+            }
+            Console.ReadLine();
         }
     }
 }
+
